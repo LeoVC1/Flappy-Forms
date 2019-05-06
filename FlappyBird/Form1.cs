19e87b7b7a200fe64ba28ec4af26b1268ff7a843
LEO VITOR DE CAMPOS <180976@facens.br>
2019-05-06 11:02:00 -0300

    Codes

--- FlappyBird/Form1.cs
+++ FlappyBird/Form1.cs
@@ -12,9 +12,50 @@ namespace FlappyBird
 {
     public partial class Form1 : Form
     {
+        private Timer t;
+        private int index = 0;
+        Brush[] brushes = new Brush[10];
+        Random rnd = new Random(DateTime.Now.Millisecond);
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.Paint += new PaintEventHandler(GameLoop);
+            t = new Timer();
+            t.Interval = 10;
+            t.Tick += new EventHandler(t_Ticket);
+
+            t.Start();
+        }
+
+        private void t_Ticket(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void GameLoop(object sender, PaintEventArgs e)
+        {
+            ResetGame();
+        }
+        private Brush newColor()
+        {
+            int r = rnd.Next(0, byte.MaxValue + 1);
+            int g = rnd.Next(0, byte.MaxValue + 1);
+            int b = rnd.Next(0, byte.MaxValue + 1);
+            Brush brush = new SolidBrush(Color.FromArgb(r, g, b));
+            return brush;
+        }
+
+        private void ResetGame()
+        {
+            
+        }
+
+
     }
 }
